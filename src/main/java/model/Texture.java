ba4222c62c9ca7d995df92c325e0aa5c21ed87c2
Jojoteur <48619415+Jojoteur@users.noreply.github.com>
2020-01-09 10:20:02 +0100

    Update Texture.java

--- src/main/java/model/Texture.java
+++ src/main/java/model/Texture.java
@@ -25,19 +25,19 @@ public class Texture{
         checkAndAddTexture("wall", "wall.jpg", imgSize);
         checkAndAddTexture("treasure", "treasure.jpg", imgSize);
         checkAndAddTexture("trap", "trap.jpg", imgSize);
-        checkAndAddTexture("hero_LEFT", "hero_left.jpg", imgSize);
-        checkAndAddTexture("hero_RIGHT", "hero_right.jpg", imgSize);
-        checkAndAddTexture("hero_UP", "hero_up.jpg", imgSize);
-        checkAndAddTexture("hero_DOWN", "hero_down.jpg", imgSize);
-        checkAndAddTexture("goblin_LEFT", "goblin_left.jpg", imgSize);
-        checkAndAddTexture("goblin_RIGHT", "goblin_right.jpg", imgSize);
-        checkAndAddTexture("goblin_UP", "goblin_up.jpg", imgSize);
-        checkAndAddTexture("goblin_DOWN", "goblin_down.jpg", imgSize);
-        checkAndAddTexture("skeleton_LEFT", "skeleton_left.jpg", imgSize);
-        checkAndAddTexture("skeleton_RIGHT", "skeleton_right.jpg", imgSize);
-        checkAndAddTexture("skeleton_UP", "skeleton_up.jpg", imgSize);
-        checkAndAddTexture("skeleton_DOWN", "skeleton_down.jpg", imgSize);
-        checkAndAddTexture("healObject", "heal_object.jpg", imgSize);
+        checkAndAddTexture("hero_LEFT", "hero_LEFT.jpg", imgSize);
+        checkAndAddTexture("hero_RIGHT", "hero_RIGHT.jpg", imgSize);
+        checkAndAddTexture("hero_UP", "hero_UP.jpg", imgSize);
+        checkAndAddTexture("hero_DOWN", "hero_DOWN.jpg", imgSize);
+        checkAndAddTexture("goblin_LEFT", "goblin_LEFT.jpg", imgSize);
+        checkAndAddTexture("goblin_RIGHT", "goblin_RIGHT.jpg", imgSize);
+        checkAndAddTexture("goblin_UP", "goblin_UP.jpg", imgSize);
+        checkAndAddTexture("goblin_DOWN", "goblin_DOWN.jpg", imgSize);
+        checkAndAddTexture("skeleton_LEFT", "squeleton_LEFT.jpg", imgSize);
+        checkAndAddTexture("skeleton_RIGHT", "squeleton_RIGHT.jpg", imgSize);
+        checkAndAddTexture("skeleton_UP", "squeleton_UP.jpg", imgSize);
+        checkAndAddTexture("skeleton_DOWN", "squeleton_DOWN.jpg", imgSize);
+        checkAndAddTexture("healObject", "healObject.jpg", imgSize);
     }
 
     public static Image getDefault()
