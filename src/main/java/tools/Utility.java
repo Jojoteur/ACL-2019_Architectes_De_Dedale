a08f3a080d23505c23611daa54e207e6335f054d
Jojoteur <guillaumece67@gmail.com>
2020-01-09 02:41:25 +0100

    import inutiles retirés

--- src/main/java/tools/Utility.java
+++ src/main/java/tools/Utility.java
@@ -7,8 +7,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URISyntaxException;
 
 import javax.imageio.ImageIO;
 
